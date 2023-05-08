9ee2111f64ca8ae5f762a278468eb467c42e1634
MaximilianMMM <125987812+MaximilianMMM@users.noreply.github.com>
2023-05-08 18:31:01 +0300

    main.c

--- main.c
+++ main.c
@@ -1,19 +1,16 @@
 #include "output.c"
 
-int main() {
+void main() {
     int num_books;
-
+    int oldest_i = 0, youngest_i = 0, longest_title_i = 0;
     books *arr;
 
+    num_books = find_n();
+    create(&num_books, &arr);
     input(num_books, arr);
-
-    int oldest_i = 0, youngest_i = 0, longest_title_i = 0;
     calculate(num_books, &oldest_i, &youngest_i, &longest_title_i, arr);
-
     output_f(&oldest_i, &youngest_i, &longest_title_i, arr);
     output_CMD();
 
     free(arr);
-
-    return 0;
 }
