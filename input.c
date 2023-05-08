681964e0442f117659196d774e3e4df166f9339d
MaximilianMMM <125987812+MaximilianMMM@users.noreply.github.com>
2023-05-08 18:30:30 +0300

    input.c

--- input.c
+++ input.c
@@ -1,11 +1,19 @@
 #include "create.c"
 
-void input(int num_books, books *arr){
+int find_n() {
+    char ch[1000];
+    int counter = 0;
+    FILE *f_in = fopen("input.txt", "r");
+    while(fgets(ch, sizeof(ch), f_in) != NULL){
+        counter++;
+    }
+    fclose(f_in);
+    return counter;
+}
 
+void input(int num_books, books *arr){
     FILE *f_in_txt = fopen("input.txt", "r");
     if(f_in_txt){
-        fscanf(f_in_txt, "%d", &num_books);
-        create(num_books, &arr);
 
         for (int i = 0; i < num_books; i++){
             fscanf(f_in_txt, "%s %s %s %d", arr[i].title, arr[i].author.surname, arr[i].author.name, &arr[i].year);
