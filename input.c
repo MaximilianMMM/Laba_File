4ec9c6aa23d757751d7f18e799796fd8a9be0e83
MaximilianMMM <125987812+MaximilianMMM@users.noreply.github.com>
2023-05-08 18:10:19 +0300

    input.c

--- input.c
+++ input.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
 #include "create.c"
 
 void input(int num_books, books *arr){
-    for (int i = 0; i < num_books; i++) {
-        printf("Введіть назву книжки №%d: ", i+1);
-        scanf("%s", arr[i].title);
-        printf("Введіть прізвище та імʼя автора книжки №%d: ", i+1);
-        scanf("%s", arr[i].author.surname);
-        scanf("%s", arr[i].author.name);
-        printf("Введіть рік в якому була опублікована книжка №%d: ", i+1);
-        scanf("%d", &arr[i].year);
-    }
+
+    FILE *f_in_txt = fopen("input.txt", "w");
+    if(f_in_txt){
+        fscanf(f_in_txt, "%d", &num_books);
+        create(num_books, &arr);
+
+        for (int i = 0; i < num_books; i++){
+            fscanf(f_in_txt, "%s %s %s %d", arr[i].title, arr[i].author.surname, arr[i].author.name, &arr[i].year);
+        }
+        fclose(f_in_txt);
+    } else printf("Помилка при вводі тесту в файлі");
+
 }
