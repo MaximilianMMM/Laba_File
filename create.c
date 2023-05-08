29a8a5ee0ef296e544efec2f14fa075cda6d9ea8
MaximilianMMM <125987812+MaximilianMMM@users.noreply.github.com>
2023-05-08 18:02:35 +0300

    create.c

--- create.c
+++ create.c
@@ -1,6 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "struct.c"
 
 void create(int num_books, books **arr){
     *arr = (books*)malloc(num_books*sizeof(books));
+}
+
+void arr_binary(books *arr, int num_books){
+    FILE *f_bin = fopen("arr_bin.bin", "wb");
+    if(f_bin){
+        fwrite(&arr, sizeof(books), num_books, f_bin);
+        fclose(f_bin);
+    }
+    else printf("Помилка при переписанні в бінарний файл");
+}
+
+void read_binary(books *arr, int num_books){
+    FILE *f_bin = fopen("arr_bin.bin", "rb");
+    if(f_bin){
+        fread(&arr, sizeof(books), num_books, f_bin);
+    }
+    else printf("Помилка при читанні бінарного файлу");
 }
